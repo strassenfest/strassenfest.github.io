99e284d8b84ecd980701c1ff67162a4aa65cd22b
pip-eline <pip-eline>
2023-05-18 11:16:39 +0200

    w

---
+++ script.js
@@ -0,0 +1,347 @@
+var data = [
+    {
+        id: `a-algemene-broadcast`,
+        nederlands: {
+            titel: `Ga in de ALGEMENE BROADCAST`,
+            beschrijving: `Dat is <a href="https://t.me/+-1BG92MkKMk2ODc0">dit telegramkanaal</a> met de titel "Stop Sub #12".`,
+            toelichting: `Voor de veel acties, en ook voor deze <span class="a-12">A12-actie</span>, is er een grote <em>signal of telegram-groep</em> waar belangrijke mededelingen worden gepost.`,
+        },
+        engels: {
+            titel: `Join the GENERAL BROADCAST`,
+            beschrijving: `It's <a href="#">this telegram group</a> with the title "Stop Sub #12".`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-vingergroep`,
+        nederlands: {
+            titel: `Ga in de VINGERGROEP`,
+            beschrijving: `Dat is de <em>Pinky signal-groep</em> waar je als het goed is al in zit :)`,
+            toelichting: `Bij grote acties wordt er soms gewerkt met 'vingers': dat zijn groepen die samen voorbereiden en naar de actielocatie lopen. Dit vergroot de veiligheid en de kans dat zo veel mogelijk rebellen op de actie aankomen.`,
+        },
+        engels: {
+            titel: `Join the FINGER GROUP`,
+            beschrijving: `It is the <em>Pinky signal-group</em> that you probably are in already :)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-actietraining`,
+        nederlands: {
+            titel: `Volg een ACTIETRAINING`,
+            beschrijving: `<a href="https://extinctionrebellion.nl/events/?organizer&category=Actietraining">Hier staat een overzicht</a> van alle actietrainingen die nog gegeven worden.`,
+            toelichting: `Dit heet ook wel een "non violent direct action training". Als je dat al eerder hebt gedaan, hoeft dat niet nogmaals. Maar als je nieuw bent, is een actietraining heel belangrijk.`,
+        },
+        engels: {
+            titel: `Do an ACTION TRAINING`,
+            beschrijving: `<a href="#">Here is an overview</a> of all the trainings being organised.`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-legal-handout`,
+        nederlands: {
+            titel: `Lees de LEGAL HANDHOUT`,
+            beschrijving: `De legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM">staat hier</a>.`,
+            toelichting: `Hierin staan antwoorden op vragen zoals: Wat zijn de legale consequenties? Wanneer bel ik een advocaat? Betekent dit iets voor mijn strafblad?`,
+        },
+        engels: {
+            titel: `Read the LEGAL HANDOUT`,
+            beschrijving: `You may <a href="#">find it here</a>.`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-risico-niveau`,
+        nederlands: {
+            titel: `Bepaal: HIGH RISK of LOW RISK`,
+            beschrijving: `Twijfel je? Bespreek de voor- en nadelen met anderen.`,
+            toelichting: `Het is prettig om een "Plan A" te hebben. Zodat je weet wat je wil, ook in hectische situaties.<br/>Bij low risk zeg je: "ik ga weg als de politie vordert". De kans dat je gearresteerd wordt, is dan heel klein.`,
+        },
+        engels: {
+            titel: `Commit: HIGH RISK or LOW RISK`,
+            beschrijving: `You can discuss this with others if you're still on the fence.`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-anonimiteit`,
+        nederlands: {
+            titel: `Bepaal: ANONIEM of niet?`,
+            beschrijving: `Er zijn diverse voor- en nadelen dus laat je vooral goed informeren.`,
+            toelichting: `Als je anoniem gaat, neem dan ook geen spullen mee met jou naam er op. Dus ook geen bankpas of OV-chipkaart op naam.`,
+        },
+        engels: {
+            titel: `Commit: HIGH RISK or LOW RISK`,
+            beschrijving: `There are various pros and cons for anonymity, so inform yourself well.`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-algemene-briefing`,
+        nederlands: {
+            titel: `Volg de ALGEMENE BRIEFING`,
+            beschrijving: `Dit is een online zoom-meeting en duurt een uur.<br/>Engels: di 23 mei 20:30. Nederlands: wo 24 mei 20:30. Een zoom-link volgt op de dag zelf, via de <em>algemene broadcast</em>.`,
+            toelichting: `In de algemene briefing geeft een laatste update en herhaalt de belangrijkste punten. Het is een zoom-meeting met honderden deelnemers, dus er is niet veel ruimte voor vragen.`,
+        },
+        engels: {
+            titel: `(translation TODO)`,
+            beschrijving: `(translation TODO)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-vingerbriefing`,
+        nederlands: {
+            titel: `Volg de VINGERBRIEFING`,
+            beschrijving: `Dinsdag 23 en woensdag 24 mei om 21:30, direct aansluitend aan de <em>algemene briefing</em>, doen wij onze eigen vingerbriefing. Een link volgt op de dag zelf. We doen de meeting twee keer, zodat je kan kiezen welke dag jou het beste uitkomt.`,
+            toelichting: `In deze briefing bespreken we het plan voor onze vinger, waaronder de verzamellocatie en aanlooproute. Bovendien is er tijd voor vragen, en kunnen er nog buddies gemaakt worden.`,
+        },
+        engels: {
+            titel: `(translation TODO)`,
+            beschrijving: `(translation TODO)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-rst-nummer`,
+        nederlands: {
+            titel: `Trek een RST-NUMMER`,
+            beschrijving: `Er wordt nog een link gepost waar je een nummer kan trekken. Dat nummer is jouw arrestanten-nummer of RST ("arrestee")-nummer. Doe dit altijd, ook als je low-risk gaat.`,
+            toelichting: `Je RST-nummer zorgt er voor dat arrestee-support jou kan traceren. Zo weten ze of er nog rebellen vast zitten. Achter de schermen werkt RST-support er hard aan om iedereen weer vrij te krijgen.`,
+        },
+        engels: {
+            titel: `(translation TODO)`,
+            beschrijving: `(translation TODO)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-tas-inpakken`,
+        nederlands: {
+            titel: `Pak je tas in`,
+            beschrijving: `Een paklijst wordt gedeeld tijdens de <em>algemene briefing</em>.`,
+            toelichting: `Denk in elk geval aan: warme kleren, eten, drinken, geld, ID-kaart*, OV-kaart*. Géén dierbare bezittingen. Géén wapens, géén drugs.<br/>* niet als je anoniem gaat`,
+        },
+        engels: {
+            titel: `(translation TODO)`,
+            beschrijving: `(translation TODO)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-plan-treinreis`,
+        nederlands: {
+            titel: `Plan je (trein)reis`,
+            beschrijving: `Een reisadvies volgt.`,
+            toelichting: `Houd rekening met eventuele vertragingen. Reis in kleine groepjes.`,
+        },
+        engels: {
+            titel: `(translation TODO)`,
+            beschrijving: `(translation TODO)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-watervaste-stift`,
+        nederlands: {
+            titel: `Schrijf dit op je lichaam`,
+            beschrijving: `Schrijf met <em>watervaste stift</em> twee dingen duidelijk op je lichaam:<br/>1. naam van de advocaat.<br/>2. Je RST-nummer.`,
+            toelichting: `De naam van de advocaat wordt later bekendgemaakt.`,
+        },
+        engels: {
+            titel: `(translation TODO)`,
+            beschrijving: `(translation TODO)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+    {
+        id: `a-zen`,
+        nederlands: {
+            titel: `Je bent er klaar voor!`,
+            beschrijving: `Neem nog wat rust. Bedenk waarom je het ook alweer doet. We hebben een goed verhaal, we zijn met z'n duizenden. Misschien vind je het spannend. Maar het wordt ook heel gaaf.`,
+            toelichting: `Tot snel :)`,
+        },
+        engels: {
+            titel: `(translation TODO)`,
+            beschrijving: `(translation TODO)`,
+            toelichting: `(translation TODO)`,
+        }
+    },
+]
+
+function start()
+{
+    let container = document.getElementById("actiepunten");
+
+    container.innerHTML = ""; // bye
+
+    let n = data.length;
+
+    for (let i = 0; i < n; i++)
+    {
+        var html = template_actiepunt(data[i].id, data[i].nederlands.titel, data[i].nederlands.beschrijving, data[i].nederlands.toelichting);
+        container.insertAdjacentHTML("beforeend", html);
+
+        if (i != n-1)
+        {
+            container.insertAdjacentHTML("beforeend", template_pijl());
+        }
+    }
+}
+
+function update()
+{
+    let items = document.getElementsByClassName("actiepunt");
+
+    let is_visible = true;
+    let i = 0;
+
+    for (let item of items) {
+
+        let is_checked = item.querySelector(".checkbox").checked;
+
+        // Show or hide the element.
+        if (is_visible)
+        {
+            item.classList.remove("hidden");
+            item.getElementsByClassName("checkbox")[0].disabled = false;
+            if (is_checked)
+            {
+                item.classList.add("done");
+            }
+            else
+            {
+                item.classList.remove("done");
+            }
+        }
+        else
+        {
+            item.classList.add("hidden");
+            item.getElementsByClassName("checkbox")[0].disabled = true;
+
+            item.classList.remove("done");
+        }
+
+        // When we meet an unchecked item, disable visibility for all next items.
+        if (!is_checked)
+        {
+            is_visible = false;
+        }
+
+        i++;
+    };
+}
+
+function clear()
+{
+    let items = document.getElementsByClassName("actiepunt");
+
+    for (let item of items) {
+        checkbox = item.querySelector(".checkbox");
+        checkbox.checked = false;
+    }
+}
+
+function reset()
+{
+    clear();
+    save_to_cookies();
+
+    start();
+    update();
+
+    document.documentElement.scrollTop = 0;
+}
+
+function save_to_cookies()
+{
+    let items = document.getElementsByClassName("actiepunt");
+
+    for (let item of items) {
+        checkbox = item.querySelector(".checkbox");
+        let is_checked = checkbox.checked;
+
+        set_cookie(String(checkbox.id), String(is_checked));
+    }
+}
+
+function load_from_cookies()
+{
+    let items = document.getElementsByClassName("actiepunt");
+
+    for (let item of items) {
+        checkbox = item.querySelector(".checkbox");
+
+        let v = get_cookie(checkbox.id);
+
+        if (v == String(true))
+        {
+            checkbox.checked = true;
+        }
+        else
+        {
+            checkbox.checked = false;
+        }
+    }
+}
+
+// Simply copy-pasting this from w3schools:
+
+function set_cookie(cname, cvalue) {
+    let days = 40;
+    const d = new Date();
+    d.setTime(d.getTime() + (days*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function get_cookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+function template_actiepunt(id, titel, beschrijving, toelichting)
+{
+    return `<div class="actiepunt" id=${id}>
+        <div class="box">
+        <p class="titel titelfont">${titel}</p>
+        <p class="beschrijving">${beschrijving}</p>
+        <div class="check">
+            <input class="checkbox" type="checkbox" id="${id}" onchange="save_to_cookies(); update()">
+            <label class="titelfont" for="${id}">Check</label>
+        </div>
+        </div>
+        <div class="inklapbaar">
+        <div class="box toevoeging">
+            <p class="toelichting">${toelichting}</p>
+        </div>
+        </div>
+    </div>`
+}
+
+
+
+function template_pijl()
+{
+    return `
+        <img class="pijl" src="img/arrow_down_thin_120.png">`
+}
+
+
+window.addEventListener('load', function () {
+    start();
+    load_from_cookies();
+    update();
+  })
